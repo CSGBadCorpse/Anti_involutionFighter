ab8dc6d135ba6ba5264603f641bb11195186e1ee
sunzhongshao <877120376@qq.com>
2023-09-13 17:01:19 +0800

    新增一些基础工具类

---
+++ Src/Lib/Common/Data/MapDefine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.Data
+{
+    public class MapDefine
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Resource { get; set; }
+        public string Music { get; set; }
+        public string MiniMap { get; set; }
+    }
+}
